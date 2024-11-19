8ba42b4556ba33aa68665fdb8e7d8a30dba87a9e
Natalya Goldsmith <ngol1755@students.vsu.edu>
2024-11-19 15:24:23 -0500

    doc: added documentation for count

--- main.py
+++ main.py
@@ -14,7 +14,14 @@ def test_return_four_for_two_and_two():
     assert add(2, 2) == 4
 
 def count(string):
+    """ returns the lenght of the string passed
+
+    Args:
+         string: the string the count or return the lenght of.
+    Returns:
+        the length of the string.
+    """
     return len(string)
-    
+
 def test_return_lenght_five_for_hello():
     assert count("hello") == 5
